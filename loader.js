f0b85980234cf5c883b6b397125e134da7b845b2
nu-el-cloud <kofoworolaemmanuel106@gmail.com>
2025-07-24 19:36:36 +0100

    Add files via upload

---
+++ loader.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+        document.body.classList.add("loading");
+    window.addEventListener("load", () => {
+        const loader = document.getElementById("loader");
+        if (loader) {
+             loader.classList.add("hidden");
+            setTimeout(() => {
+                loader.style.display = "none";
+            }, 500);
+        }
+        document.body.classList.remove("loading");
+        document.body.classList.add("loaded");
+    });
+});
